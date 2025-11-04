079ccaccb45a6a84fd234c9f0252688a8eb606da
SkimerPM <jose.sanchez.q@tecsup.edu.pe>
2025-11-04 11:34:49 -0500

    comit 2

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,14 +82,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'universitario',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
